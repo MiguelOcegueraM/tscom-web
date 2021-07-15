bd1cbed34ae3967846a830e17889576ad8ed39cf
MiguelOcegueraM <migueloceguera.m@gmail.com>
2021-07-14 19:40:03 -0500

    I started with the php order

---
+++ footer.php
@@ -0,0 +1,53 @@
+
+
+    <footer class="section footer-classic section-sm">
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-lg-3 wow fadeInLeft">
+              <!--Brand--><a class="brand" href="index.html"><img class="brand-logo-dark" src="images/logo-default-200x34.png" alt="" width="100" height="17"/><img class="brand-logo-light" src="images/tscom-logo-web.png" alt="" width="100" height="17"/></a>
+              <p class="footer-classic-description offset-top-0 offset-right-25">СonsultBiz provides a full range of business consulting & advisory services to small, medium, and international companies worldwide. We use innovations and experience to drive your success.</p>
+            </div>
+            <div class="col-lg-3 col-sm-8 wow fadeInUp">
+              <P class="footer-classic-title">CONTACTO</P>
+              <a class="d-block offset-top-0" href="https://www.google.com/maps/place/TSCOM+tecnologia+en+sistemas+de+comunicacion/@19.2540234,-103.7269423,15z/data=!4m5!3m4!1s0x0:0xd6c949c3d443181e!8m2!3d19.2540075!4d-103.7269143">Rafaela Suárez #455, Col. Magisterial<span class="d-lg-block">Colima 28030, México</span></a><a class="d-inline-block" href="tel:+523123146309">+52 312 314 63 09</a>
+              <ul>
+                <li>Lun-Vie:<span class="d-inline-block offset-left-10 text-white">9:00 AM - 5:00 PM</span></li>
+                <li>Sáb:<span class="d-inline-block offset-left-10 text-white">9:00 AM - 1:00 PM</span></li>
+              </ul>
+              <a class="d-inline-block d-block offset-top-0" href="mailto:atencionaclientes@tscomunicacion.com">atencionaclientes@tscomunicacion.com</a>
+            </div>
+            <div class="col-lg-2 col-sm-4 wow fadeInUp" data-wow-delay=".3s">
+              <P class="footer-classic-title">VISITA: </P>
+              <ul class="footer-classic-nav-list">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="about.php">Nosotros</a></li>
+                <li><a href="services.php">Servicios</a></li>
+                <li><a href="contacts.php">Contacto</a></li>
+              </ul>
+            </div>
+            <div class="col-lg-4 wow fadeInLeft" data-wow-delay=".2s">
+              <P class="footer-classic-title">Ofertas</P>
+              <form class="rd-mailform text-left footer-classic-subscribe-form" data-form-output="form-output-global" data-form-type="contact" method="post" action="bat/rd-mailform.php">
+                <div class="form-wrap">
+                  <label class="form-label" for="subscribe-email">Correo electrónico</label>
+                  <input class="form-input" id="subscribe-email" type="email" name="email" data-constraints="@Email @Required">
+                </div>
+                <div class="form-button group-sm text-center text-lg-left">
+                  <button class="button button-primary button-circle" type="submit">Aceptar</button>
+                </div>
+              </form>
+              <p>Conoce acerca de nuestro servicios y recibe ofertas con descuentos.</p>
+            </div>
+          </div>
+        </div>
+        <div class="container wow fadeInUp" data-wow-delay=".4s">
+          <div class="footer-classic-aside">
+            <p class="rights"><span>&copy;&nbsp;</span><span class="copyright-year"></span>. Derechos Reservados. Design by <a href="">TSCOM Software Consultancy</a></p>
+            <ul class="social-links">
+              <li><a class="fa fa-facebook" href="https://www.facebook.com/TscomMx/"></a></li>
+              <li><a class="fa fa-instagram" href="https://www.instagram.com/tscom_mx/"></a></li>
+            </ul>
+          </div>
+        </div>
+    </footer>
+    
---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html class="wide wow-animation" lang="en">
+  <head>
+    <title>Inicio</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Montserrat:300,400,700%7CPoppins:300,400,500,700,900">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/fonts.css">
+    <link rel="stylesheet" href="css/style.css">
+    <style>.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}</style>
+  </head>
+  <body>
+    <!--Preloader-->
+    <?php include("preloader.php")?>
+    <div class="page">
+    <?php include("header.php")?>
+      <!--Main section-->
+      <section class="section main-section parallax-scene-js" style="background:url('images/bg-1-1700x803.jpg') no-repeat center center; background-size:cover;">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-xl-8 col-12">
+              <div class="main-decorated-box text-center text-xl-left">
+                <h1 class="text-white text-xl-center wow slideInRight" data-wow-delay=".3s"><span class="align-top offset-top-30 d-inline-block font-weight-light prefix-text"></span><span class="big font-weight-bold d-inline-flex offset-right-170">Soluciones</span><span class="biggest d-block d-xl-flex font-weight-bold">Integrales.</span></h1>
+              </div>
+            </div>
+            <div class="col-12 text-center offset-top-75" data-wow-delay=".2s"><a class="button-way-point d-inline-block text-center d-inline-flex flex-column justify-content-center" href="#" data-custom-scroll-to="about"><span class="fa-chevron-down"></span></a></div>
+          </div>
+        </div>
+        <div class="decorate-layer">
+          <div class="layer-1">
+            <div class="layer" data-depth=".20"><img src="images/parallax-item-1-563x532.png" alt="" width="563" height="266"/>
+            </div>
+          </div>
+          <div class="layer-2">
+            <div class="layer" data-depth=".30"><img src="images/parallax-item-2-276x343.png" alt="" width="276" height="171"/>
+            </div>
+          </div>
+          <div class="layer-3">
+            <div class="layer" data-depth=".40"><img src="images/parallax-item-3-153x144.png" alt="" width="153" height="72"/>
+            </div>
+          </div>
+          <div class="layer-4">
+            <div class="layer" data-depth=".20"><img src="images/parallax-item-4-69x74.png" alt="" width="69" height="37"/>
+            </div>
+          </div>
+          <div class="layer-5">
+            <div class="layer" data-depth=".40"><img src="images/parallax-item-5-72x75.png" alt="" width="72" height="37"/>
+            </div>
+          </div>
+          <div class="layer-6">
+            <div class="layer" data-depth=".30"><img src="images/parallax-item-6-45x54.png" alt="" width="45" height="27"/>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--About-->
+      <section class="section section-sm position-relative" id="about">
+        <div class="container">
+          <div class="row row-30">
+            <div class="col-lg-6">
+              <div class="block-decorate-img wow fadeInLeft" data-wow-delay=".2s"><img src="images/home-1-570x703.jpg" alt="" width="570" height="351"/>
+              </div>
+            </div>
+            <div class="col-lg-6 col-12">
+              <div class="block-sm offset-top-45">
+                <div class="section-name wow fadeInRight" data-wow-delay=".2s">Nosotros</div>
+                <h3 class="wow fadeInLeft text-capitalize devider-bottom" data-wow-delay=".3s">Que<span class="text-primary"> Hacemos</span></h3>
+                <p class="offset-xl-40 wow fadeInUp" data-wow-delay=".4s">Somos una empresa colimense con mas de 15 años de experiencia ofreciendo soluciones para nuestros clientes con servicios de radiocomunicación, videovigilancia con Inteligencia Artificial, sistemas de geolocalización, control de acceso industrial y residencial, equipamiento de unidades de emergencia y redes inalámbricas.</p>
+                <p class="default-letter-spacing font-weight-bold text-gray-dark wow fadeInUp" data-wow-delay=".4s">Buscamos siempre realizar servicios de alta calidad implementando soluciones con tecnologías disruptivas.</p><a class="button-width-190 button-primary button-circle button-lg button offset-top-30" href="#">Ver más</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="decor-text decor-text-1">CONÓCENOS</div>
+      </section>
+      <!--Features-->
+      <section class="section custom-section position-relative section-md">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-7 col-lg-7 col-12">
+              <div class="section-name wow fadeInRight">Valores</div>
+              <h3 class="text-capitalize devider-left wow fadeInLeft" data-wow-delay=".2s">Porque nuestros clientes <span class="text-primary"> nos eligen</span></h3>
+              <p>Contamos con mas de 15 años de experiencia creando soluciones para nuestros clientes teniendo en cuenta 4 pilares importantes:</p>
+              <div class="row row-15">
+                <div class="col-xl-6 wow fadeInUp" data-wow-delay=".2s">
+                  <div class="box-default">
+                    <div class="box-default-title">Soluciones inovadoras</div>
+                    <p class="box-default-description">Nos gusta ofrecer soluciones con tecnologías disruptivas.</p><span class="box-default-icon novi-icon icon-lg mercury-icon-lightbulb-gears"></span>
+                  </div>
+                </div>
+                <div class="col-xl-6 wow fadeInUp" data-wow-delay=".3s">
+                  <div class="box-default">
+                    <div class="box-default-title">pensamiento estratégico</div>
+                    <p class="box-default-description">Utilizamos los recursos y el tiempo de manera óptima.</p><span class="box-default-icon novi-icon icon-lg mercury-icon-target-2"></span>
+                  </div>
+                </div>
+                <div class="col-xl-6 wow fadeInUp" data-wow-delay=".4s">
+                  <div class="box-default">
+                    <div class="box-default-title">experiencia</div>
+                    <p class="box-default-description">Contamos con personal capacitado para atender a nuestros clientes.</p><span class="box-default-icon novi-icon icon-lg mercury-icon-user"></span>
+                  </div>
+                </div>
+                <div class="col-xl-6 wow fadeInUp" data-wow-delay=".5s">
+                  <div class="box-default">
+                    <div class="box-default-title">seguridad y confidencialidad</div>
+                    <p class="box-default-description">Cuidamos la integridad de la información de nuestros clientes.</p><span class="box-default-icon novi-icon icon-lg mercury-icon-partners"></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="image-left-side wow slideInRight" data-wow-delay=".5s"><img src="images/home-2-636x480.jpg" alt="" width="636" height="240"/>
+        </div>
+        <div class="decor-text decor-text-2 wow fadeInUp" data-wow-delay=".3s">Valores</div>
+      </section>
+  
+      <!--Owl Carousel-->
+      <section class="section section-md bg-gray-lighten">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-xl-10">
+              <div class="section-name wow fadeInRight text-center" data-wow-delay=".2s">Testimonios</div>
+              <h3 class="wow fadeInLeft text-capitalize text-center" data-wow-delay=".3s">Lo Que Nuestros Clientes<span class="text-primary"> Opinan</span></h3>
+              <div class="owl-carousel review-carousel" data-items="1" data-sm-items="1" data-md-items="1" data-lg-items="1" data-xl-items="1" data-xxl-items="1" data-dots="true" data-nav="false" data-stage-padding="0" data-loop="false" data-margin="0" data-mouse-drag="true" data-autoplay="false">
+                <div class="item">
+                  <div class="item-preview wow fadeInDown" data-wow-delay=".2s"><img src="images/user-1-216x216.jpg" alt="" width="216" height="108"/>
+                  </div>
+                  <div class="item-description wow fadeInUp" data-wow-delay=".3s">
+                    <p>I was impressed by the quality of what your specialists did. They quickly found out what was the main problem of my company and helped me form a correct strategy, which worked perfectly and set my business on the right path.</p>
+                    <div class="item-subsection"><span class="item-subsection-title devider-left">Heather Perry</span><span>Regular Client</span></div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="item-preview wow fadeInDown" data-wow-delay=".2s"><img src="images/user-2-216x216.jpg" alt="" width="216" height="108"/>
+                  </div>
+                  <div class="item-description wow fadeInUp" data-wow-delay=".3s">
+                    <p>I appreciate what you’ve done to help my small company develop and thrive in that market niche that we specialize in. Your business consulting experts are the best in the whole industry and I’m going to recommend you to my partners!</p>
+                    <div class="item-subsection"><span class="item-subsection-title devider-left">Donald Hughes</span><span>Regular Client</span></div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="item-preview wow fadeInDown" data-wow-delay=".2s"><img src="images/user-3-216x216.jpg" alt="" width="216" height="108"/>
+                  </div>
+                  <div class="item-description wow fadeInUp" data-wow-delay=".3s">
+                    <p>My cooperation with your team helped me understand that you really care about your clients – it was proved by a lot of positive reviews already – and that's why I chose you. Thank you for a great job, you are true professionals!</p>
+                    <div class="item-subsection"><span class="item-subsection-title devider-left">Anthony Parker</span><span>Regular Client</span></div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="item-preview wow fadeInDown" data-wow-delay=".2s"><img src="images/user-4-216x216.jpg" alt="" width="216" height="108"/>
+                  </div>
+                  <div class="item-description wow fadeInUp" data-wow-delay=".3s">
+                    <p>Two years after СonsultBiz’s involvement, we are remarkably successful and the leader in served markets. This brought industry knowledge, best practices, and real-world solutions to the intricate challenges we faced.</p>
+                    <div class="item-subsection"><span class="item-subsection-title devider-left">Jennifer Coleman</span><span>Regular Client</span></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--Footer-->
+      <?php include("footer.php")?>
+    <div class="snackbars" id="form-output-global"></div>
+    <script src="js/core.min.js"></script>
+    <script src="js/script.js"></script>
+    <!--coded by Drel-->
+  </body>
+</html>
