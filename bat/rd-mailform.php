87096621123194cbd3aa39276e4f40dc1961cc13
MiguelOcegueraM <migueloceguera.m@gmail.com>
2021-07-20 12:08:02 -0500

    fixing errrors with mailer

--- bat/rd-mailform.php
+++ bat/rd-mailform.php
@@ -94,7 +94,7 @@ try {
         // 0 = off (for production use)
         // 1 = client messages
         // 2 = client and server messages
-        $mail->SMTPDebug = 1;
+        $mail->SMTPDebug = 0;
 
         $mail->Debugoutput = 'html';
 
@@ -106,7 +106,7 @@ try {
 
         // Whether to use SMTP authentication
         $mail->SMTPAuth = true;
-        $mail->SMTPSecure = "ssl";
+        $mail->SMTPSecure = "tls";
 
         // Username to use for SMTP authentication
         $mail->Username = $formConfig['username'];
